0f104f7d8c4bc5e048be94c572d8d02b69475608
OddPixle <141264961+OddPixle@users.noreply.github.com>
2024-07-19 13:36:04 +0200

    Delo na movmentu

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,28 +1,21 @@
 extends CharacterBody2D
 
-
-const SPEED = 100.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var speed=10
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
 
 
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+func _input(event):
+	if event.is_action("move_up"):
+		position.y-=speed
+	if event.is_action("move_down"):
+		position.y+=speed
+	if event.is_action("move_right"):
+		position.x+=speed
+	if event.is_action("move_left"):
+		position.x-=speed
