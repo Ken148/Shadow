9e69eb275f7917daa5573fefdceffffabb50a7bc
Ken148 <ken.turk70@gmail.com>
2024-07-19 16:27:43 +0200

    Spremenil enemy script

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -1,25 +1,25 @@
 extends CharacterBody2D
 
-var speed=10
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var speed = 200 
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-func _input(event):
-	if event.is_action("move_up"):
-		position.y-=speed
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
 		$Sprite2D/EnemyAnimation.play("Turn_backwards")
-	if event.is_action("move_down"):
-		position.y+=speed
+		
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
 		$Sprite2D/EnemyAnimation.play("Turn_forward")
-	if event.is_action("move_right"):
-		position.x+=speed
+		
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
 		$Sprite2D/EnemyAnimation.play("Turn_right")
-	if event.is_action("move_left"):
-		position.x-=speed
+		
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
 		$Sprite2D/EnemyAnimation.play("Turn_left")
+	velocity = velocity.normalized() * speed
+
+func _physics_process(delta):
+	get_input()
