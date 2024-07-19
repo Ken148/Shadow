22ef42e947bd0eb455c34822e9f98397e63f01f1
Ken148 <ken.turk70@gmail.com>
2024-07-19 16:35:41 +0200

    Update Enemy.gd

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -1,25 +1,26 @@
 extends CharacterBody2D
 
-var speed = 200 
+var speed = 200
+var velocity_ = Vector2()
 
 func get_input():
-	velocity = Vector2()
+	var direction = Vector2()
 	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
+		direction.y -= 1
 		$Sprite2D/EnemyAnimation.play("Turn_backwards")
 		
 	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
+		direction.y += 1
 		$Sprite2D/EnemyAnimation.play("Turn_forward")
 		
 	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
+		direction.x += 1
 		$Sprite2D/EnemyAnimation.play("Turn_right")
 		
 	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-		$Sprite2D/EnemyAnimation.play("Turn_left")
-	velocity = velocity.normalized() * speed
+		direction.x -= 1
+	velocity_ = direction.normalized() * speed
 
 func _physics_process(delta):
 	get_input()
+	move_and_slide(velocity_)
