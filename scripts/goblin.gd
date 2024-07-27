24538f99fcc941afa99101217a42b149bc216d2b
Ken148 <ken.turk70@gmail.com>
2024-07-27 21:05:52 +0200

    Delam za main menu povezavo za gumbe

--- scripts/goblin.gd
+++ scripts/goblin.gd
@@ -3,32 +3,35 @@ extends Node2D
 var speed = 5
 var stop_distance = 10
 var player_node: Node2D
+var enemy_node: Node2D
+@onready var enemy_body_node = $Body
+@onready var AnimationEnemy = enemy_body_node.get_node("AnimationEnemy")
 
 func _ready():
-	var animation = $Body/AnimationPlayer
+	pass
 	
 func _physics_process(delta):
-	var animation = $Body/AnimationPlayer
 	player_node = get_tree().root.get_node("/root/MainScene/Player")
-	if player_node:
-		var player_position = player_node.position
-		var enemy_position = position
-		var direction = (player_position - enemy_position).normalized()
-		var distance = player_position.distance_to(enemy_position)
+	enemy_node = get_tree().root.get_node("/root/MainScene/Enemy/Body")
+	
+	var player_position = player_node.position
+	var enemy_position = enemy_node.position
+	var direction = (player_position - enemy_position).normalized()
+	var distance = player_position.distance_to(enemy_position)
 		
-		if distance > stop_distance:
-			position += direction * delta * speed
-		else:
-			position = enemy_position
+	if distance > stop_distance:
+		position += direction * delta * speed
+	else:
+		position = enemy_position
 			
-		if abs(direction.x) > abs(direction.y):
-			if direction.x > 0:
-				animation.play("facing_right")
-			else:
-				animation.play("facing_left")
+	if abs(direction.x) > abs(direction.y):
+		if direction.x > 0:
+			AnimationEnemy.play("facing_right")
 		else:
-			if direction.y > 0:
-				animation.play("facing_forwards")
-			else:
-				animation.play("facing_backward")
+			AnimationEnemy.play("facing_left")
+	else:
+		if direction.y > 0:
+			AnimationEnemy.play("facing_forwards")
+		else:
+			AnimationEnemy.play("facing_backward")
 
--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,15 +1,27 @@
 extends Sprite2D
 
+# Declare variables to hold references to nodes
+@onready var options_button = $Options_button
+@onready var exit_button = $Exit_button
+@onready var start_button = $Start_button
+
+func _ready():
+	# Connect signals from the button nodes to their respective methods
+	exit_button.connect("pressed", self, "_on_exit_button_pressed")
+	start_button.connect("pressed", self, "_on_start_button_pressed")
+	options_button.connect("pressed", self, "_on_options_button_pressed")
 
 func _physics_process(delta):
-	$AnimationPlayer.play("Idle")
+	# Play the 'Idle' animation
+	var animation_player = $AnimationPlayer
+	if animation_player:
+		animation_player.play("Idle")
 
 func _on_exit_button_pressed():
-	get_tree().quit()
+	get_tree().quit()  # Quit the game
 
-func _on_start_buton_pressed():
-	get_tree().change_scene_to_file("res://scenes/main_scene.tscn")
-
+func _on_start_button_pressed():
+	get_tree().change_scene("res://scenes/main_scene.tscn")  # Change to the main scene
 
 func _on_options_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/settings.tscn")
+	get_tree().change_scene("res://scenes/settings.tscn")  # Change to the settings scene
