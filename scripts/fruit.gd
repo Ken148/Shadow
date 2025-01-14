a74b199b7fc8d2e79cd2a8c692d5161678fad24e
Ken148 <ken.turk70@gmail.com>
2025-01-14 01:13:48 +0100

    Update fruit.gd

--- scripts/fruit.gd
+++ scripts/fruit.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var mana_bar = get_node("/root/MainScene/Player/Camera2D/UI/Health_fill/Mana_fill")
 
+
 func _on_area_2d_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
 	# increasing the players mana
 	if (area.is_in_group("Player")):
